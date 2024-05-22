3bccd9e298e4adc1108cbabfb8d7cfbaed18bd2d
Mahek <mahekpjain@gmail.com>
2024-05-22 12:03:59 +0530

    changed main.jsx and mainComponent.jsx

--- src/component/drilling/mainComponent.jsx
+++ src/component/drilling/mainComponent.jsx
@@ -1,52 +1,51 @@
-import React, { useState, useContext } from "react";
-let context = React.createContext(null);
-import "./styles.css";
 
-function Parent() {
+import React, { useState } from "react";
+
+function MainComponent() {
 	const [fName, setfName] = useState("firstName");
 	const [lName, setlName] = useState("LastName");
 	return (
-		<context.Provider value={{ fName, lName }}>
-			<div className="parent">
-				This is a Parent component
-			</div>
-			<ChildA />
-		</context.Provider>
-	);
-}
-
-function ChildA() {
-	return (
 		<>
-			<div className="child">
-			  This is ChildA Component.
-		    </div>
-			<ChildB />
+			<div className="parent">This is a Parent component</div>
+			<br />
+			<ChildA fName={fName} lName={lName} />
 		</>
 	);
 }
 
-function ChildB() {
+function ChildA({ fName, lName }) {
 	return (
-		<div className="childB">
-			This is ChildB Component.
+		<>
+			<div className="childA">his is ChildA Component.
 			<br />
-			<ChildC />
-		</div>
+			<ChildB fName={fName} lName={lName} />
+			</div>T
+		</>
 	);
 }
 
-function ChildC() {
-	const { fName, lName } = useContext(context);
+function ChildB({ fName, lName }) {
 	return (
-		<div className="childC">
-			This is ChildC component.
+		<>
+			<div className="childB">This is ChildB Component.
+			<br />
+			<ChildC fName={fName} lName={lName} />
+			</div>
+		</>
+	);
+}
+
+function ChildC({ fName, lName }) {
+	return (
+		<>
+			<div className="childC">This is ChildC component.
 			<br />
 			<h3> Data from Parent component is as follows:</h3>
 			<h4>{fName}</h4>
 			<h4>{lName}</h4>
-		</div>
+			</div>
+		</>
 	);
 }
 
-export default Parent;
+export default MainComponent;
